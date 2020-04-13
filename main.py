f350c1b8f00f3b182105b8adcbd8b106646fa303
Kai <echorechi@gmail.com>
2020-04-13 08:59:13 +0100

    updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 import requests as rq
-from bs4 import beautyfulsoup as bs
+from bs4 import BeautifulSoup as bs
 
 
+url = "https://uk.finance.yahoo.com/quote/FB?p=FB&.tsrc=fin-srch"
+cprice = 0
+while True:
+
+	page = rq.get(url)
+	soup = bs(page.content, "html.parser")
+	bsoup = soup.prettify()
+	price = soup.find_all("span", class_="Trsdu(0.3s) Trsdu(0.3s) Fw(b) Fz(36px) Mb(-4px) D(b)")
+	rprice = price[0].get_text()
+	if cprice != rprice[0]:
+		print(price[0].get_text())
+		cprice = rprice[0]
